afddab791e1a62470c387e0bca28b3aee93556c2
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-08 09:53:48 +0100

    Fix cart counter display up to 5 digits

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -26,7 +26,7 @@ const CompanyClaim = () => (
             <div className={styles.cartIcon}>
               <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
             </div>
-            <div className={styles.cartCounter}>0</div>
+            <div className={styles.cartCounter}>00000</div>
           </a>
         </div>
       </div>
