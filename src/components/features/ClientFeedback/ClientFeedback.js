cd6abfea528b5085aa4bd438c6e1d0bd2920886b
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-11 16:29:32 +0100

    Add swipe to client feedback

--- src/components/features/ClientFeedback/ClientFeedback.js
+++ src/components/features/ClientFeedback/ClientFeedback.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 
 import styles from './ClientFeedback.module.scss';
 import { getFeedback } from '../../../redux/feedbackRedux';
+import Swipeable from '../../common/Swipeable/Swipeable';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
@@ -31,6 +32,14 @@ const ClientFeedback = () => {
     );
   }
 
+  const swipeLeftHandler = () => {
+    if (activeFeedback < feedback.length - 1)
+      setActiveFeedback(prevState => prevState + 1);
+  };
+  const swipeRightHandler = () => {
+    if (activeFeedback > 0) setActiveFeedback(prevState => prevState - 1);
+  };
+
   return (
     <div className={styles.root}>
       <div className='container mb-5'>
@@ -44,29 +53,31 @@ const ClientFeedback = () => {
             </div>
           </div>
         </div>
-        <div className='row'>
-          <div className={'col ' + styles.feedback}>
-            <p>
-              <FontAwesomeIcon icon={faQuoteRight}>x</FontAwesomeIcon>
-            </p>
-            <p className='px-5'>{feedback[activeFeedback].content}</p>
-            <div className={`row m-auto text-center ${styles.client}`}>
-              <div className='col p-0'>
-                <img
-                  className={styles.image}
-                  src={`${process.env.PUBLIC_URL}/images/feedback/${feedback[activeFeedback].photo}`}
-                  alt='client avatar'
-                />
-              </div>
-              <div className='col p-0 text-center align-middle'>
-                <p className='m-0'>{feedback[activeFeedback].name}</p>
-                <p className='m-0 small text-muted'>
-                  {feedback[activeFeedback].description}
-                </p>
+        <Swipeable leftAction={swipeLeftHandler} rightAction={swipeRightHandler}>
+          <div className='row'>
+            <div className={'col ' + styles.feedback}>
+              <p>
+                <FontAwesomeIcon icon={faQuoteRight}>x</FontAwesomeIcon>
+              </p>
+              <p className='px-5'>{feedback[activeFeedback].content}</p>
+              <div className={`row m-auto text-center ${styles.client}`}>
+                <div className='col p-0'>
+                  <img
+                    className={styles.image}
+                    src={`${process.env.PUBLIC_URL}/images/feedback/${feedback[activeFeedback].photo}`}
+                    alt='client avatar'
+                  />
+                </div>
+                <div className='col p-0 text-center align-middle'>
+                  <p className='m-0'>{feedback[activeFeedback].name}</p>
+                  <p className='m-0 small text-muted'>
+                    {feedback[activeFeedback].description}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Swipeable>
       </div>
     </div>
   );
