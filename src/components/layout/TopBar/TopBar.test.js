01f2a1a1c7c38521e80b8410f3c6c9f5372f55a7
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-18 12:22:30 +0100

    Fix test

--- src/components/layout/TopBar/TopBar.test.js
+++ src/components/layout/TopBar/TopBar.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import TopBar from './TopBar';
 
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
 describe('TopBar', () => {
+  const initialState = { output: 10 };
+  const mockStore = configureStore();
+  let store;
   it('renders without crashing', () => {
-    shallow(<TopBar />);
+    store = mockStore(initialState);
+    const component = shallow(
+      <Provider store={store}>
+        <TopBar />
+      </Provider>
+    );
+    expect(component).toBeTruthy();
   });
 });
