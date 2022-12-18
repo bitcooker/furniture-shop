8b893247dc0f935a3f86ff9032067349709ea47e
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-18 14:31:00 +0100

    WDP221202-31

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -1,29 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { getBrands } from '../../../redux/brandsRedux';
-
+import { getMode } from '../../../redux/rwdModeRedux';
 import styles from './Brands.module.scss';
 
 const Brands = () => {
   const brands = useSelector(state => getBrands(state));
+  const mode = useSelector(state => getMode(state));
+  const visibleElements = { mobile: 2, tablet: 3, desktop: 6 };
+  const [active, setActive] = useState(0);
+  const [animation, setAnimation] = useState(false);
+
+  let activeBrands = [];
+  for (let i = 0; i < brands.length; i += visibleElements[mode]) {
+    const chunk = brands.slice(i, i + visibleElements[mode]);
+    activeBrands.push(chunk);
+  }
+
+  const handleSlideChange = newSlide => {
+    setTimeout(() => {
+      setActive(newSlide);
+      setAnimation(false);
+    }, 300);
+  };
+
+  const prevPart = () => {
+    if (active > 0) {
+      handleSlideChange(active - 1);
+    }
+    if (active === 0) {
+      handleSlideChange(activeBrands.length - 1);
+    }
+    setAnimation(true);
+  };
+
+  const nextPart = () => {
+    if (active < activeBrands.length - 1) {
+      handleSlideChange(active + 1);
+    }
+    if (active === activeBrands.length - 1) {
+      handleSlideChange(0);
+    }
+    setAnimation(true);
+  };
+
   return (
     <div className='container mt-3 mb-5'>
       <div className={`border-top border-bottom ${styles.brands}`}>
-        <div className={`border m-3 ${styles.arrow}`}>
+        <div onClick={() => prevPart()} className={`border m-3 ${styles.arrow}`}>
           <span className='text-muted'>{'<'}</span>
         </div>
-        <div className='row d-flex w-100 m-3 text-center'>
-          {brands.map(item => (
-            <div key={item} className='col m-2 border'>
+        <div className='row d-flex align-item-center  m-3 text-center'>
+          {activeBrands[active].map(item => (
+            <div key={item.id} className='col border m-2'>
               <img
-                className={styles.image}
+                className={`${styles.image} ${
+                  animation ? styles.fadeOut : styles.fadeIn
+                }`}
                 src={`${process.env.PUBLIC_URL}/images/brands/${item.photo}`}
                 alt='brand logo'
               />
             </div>
           ))}
         </div>
-        <div className={`border m-3 ${styles.arrow}`}>
+        <div onClick={() => nextPart()} className={`border m-3 ${styles.arrow}`}>
           <span className='text-muted'>{'>'}</span>
         </div>
       </div>
