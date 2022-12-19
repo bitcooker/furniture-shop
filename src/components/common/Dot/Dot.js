bce0ba7632001a6ca3d0bcacbff0b7d35efc6bd2
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-19 18:34:29 +0100

    Create Dot component

---
+++ src/components/common/Dot/Dot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Dot.module.scss';
+
+const Dot = () => {
+  return <div>kroppka</div>;
+};
+
+Dot.propTypes = {};
+
+export default Dot;
