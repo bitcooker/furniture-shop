2506dab689e0b1d1986c8d3ffd8a9a9724982b56
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-30 23:17:33 +0100

    Fix bug after merge with current master

--- src/components/common/GallerySlider/Promo/Promo.js
+++ src/components/common/GallerySlider/Promo/Promo.js
@@ -3,19 +3,20 @@ import styles from './Promo.module.scss';
 import PropTypes from 'prop-types';
 import { useShowPrice } from '../../../../hooks/price-hook';
 
-const Promo = ({ topSeller }) => {
-  const priceToDisplay = useShowPrice(topSeller[2].price);
-  const priceOldToDisplay = useShowPrice(topSeller[2].priceOld);
+const Promo = ({ price, priceOld }) => {
+  const priceToDisplay = useShowPrice(price);
+  const priceOldToDisplay = useShowPrice(priceOld);
   return (
     <div className={styles.promo}>
       <h4>{priceToDisplay}</h4>
-      {topSeller[2].priceOld && <h6>{priceOldToDisplay}</h6>}
+      {priceOld && <h6>{priceOldToDisplay}</h6>}
     </div>
   );
 };
 
 Promo.propTypes = {
-  topSeller: PropTypes.object,
+  price: PropTypes.number,
+  priceOld: PropTypes.number,
 };
 
 export default Promo;
