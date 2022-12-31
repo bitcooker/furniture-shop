b7c865a9a1184b4c5eb29951723d9a27f5a3b27c
bitcooker <dreamhighpassionhigh@gmail.com>
2022-12-31 14:21:44 +0100

    add sort number of items displayed

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,5 +1,5 @@
 /* eslint-disable indent */
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getAll } from '../../../redux/productsRedux';
@@ -10,6 +10,12 @@ import ProductListItem from './ProductListItem/ProductListItem';
 const ProductList = () => {
   const { categoryId } = useParams();
   const allProducts = useSelector(getAll);
+
+  const [counter, setCounter] = useState(6);
+
+  const handleCounter = value => {
+    setCounter(value);
+  };
   return (
     <div className={styles.root}>
       <div className='container'>
@@ -23,20 +29,50 @@ const ProductList = () => {
           <div className='col-9'>
             <div className={styles.sortLine}>
               <h4>{categoryId}</h4>
+              <div className={styles.sortLineRight}>
+                <form>
+                  <label>
+                    Sort By
+                    <select>
+                      <option value='lowest'>Price: Lowest price</option>
+                      <option value='highest'>Price: Highest price</option>
+                      <option selected value='mostPurchased'>
+                        Most purchased
+                      </option>
+                      <option value='mostFavorites'>Most Favorites</option>
+                    </select>
+                  </label>
+                </form>
+                <form onChange={e => handleCounter(e.target.value)}>
+                  <label>
+                    Show
+                    <select>
+                      <option selected value='6'>
+                        6
+                      </option>
+                      <option value='12'>12</option>
+                      <option value='24'>24</option>
+                    </select>
+                  </label>
+                </form>
+              </div>
             </div>
-            {allProducts.map(product => (
-              <ProductListItem
-                key={product.id}
-                name={product.name}
-                category={product.category}
-                price={product.price}
-                image={product.image}
-                priceOld={product.priceOld}
-                id={product.id}
-                userRating={product.userRating}
-                stars={product.stars}
-              />
-            ))}
+            {allProducts.map(
+              (product, index) =>
+                index < counter && (
+                  <ProductListItem
+                    key={product.id}
+                    name={product.name}
+                    category={product.category}
+                    price={product.price}
+                    image={product.image}
+                    priceOld={product.priceOld}
+                    id={product.id}
+                    userRating={product.userRating}
+                    stars={product.stars}
+                  />
+                )
+            )}
           </div>
           <div className='col-3'>
             <h5>Filter by categories</h5>
